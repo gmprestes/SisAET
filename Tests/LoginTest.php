fd75a9b2d40de869af2f6d5d243bc95af4213747
Guilherme M. Prestes da Silva <gmprestes@gmail.com>
2016-01-05 00:58:21 -0200

    Teste da build certo

--- Tests/LoginTest.php
+++ Tests/LoginTest.php
@@ -4,7 +4,7 @@ class LoginTest extends PHPUnit_Framework_TestCase
     public function testEmpty()
     {
         $stack = array();
-        array_push($stack,1);
+        //array_push($stack,1);
         $this->assertEmpty($stack);
 
         return $stack;
