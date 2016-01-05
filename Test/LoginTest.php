b671b88a7fcd430fae0d67bf938eeb3cf475bb8c
Guilherme M. Prestes da Silva <gmprestes@gmail.com>
2016-01-05 00:49:12 -0200

    Teste se falha o build

--- Test/LoginTest.php
+++ Test/LoginTest.php
@@ -4,6 +4,7 @@ class StackTest extends PHPUnit_Framework_TestCase
     public function testEmpty()
     {
         $stack = array();
+        array_push($stack,1);
         $this->assertEmpty($stack);
 
         return $stack;
