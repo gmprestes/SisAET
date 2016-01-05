a9ec77951a349ed9fb77b3a05441b863a9833e65
Guilherme M. Prestes da Silva <gmprestes@gmail.com>
2016-01-05 00:29:09 -0200

    Test Case

---
+++ Test/LoginTest.php
@@ -0,0 +1,11 @@
+<?php
+class StackTest extends PHPUnit_Framework_TestCase
+{
+    public function testEmpty()
+    {
+        $stack = array();
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+  }
